9338dda4fc6b626db8445eb31dcf60d4ca5d2a4a
Paulo Henrique Pereira Oliveira <paulohenrique81dev@gmail.com>
2023-01-18 08:30:43 -0300

    Primeira aula

---
+++ web/src/App.tsx
@@ -0,0 +1,17 @@
+import './styles/global.css';
+import { Habit } from "./components/Habit"
+
+function App() {
+  return (
+    <div>
+      <Habit completed={3} />
+      <Habit completed={10} />
+      <Habit completed={8} />
+      <Habit completed={3} />
+      <Habit completed={4} />  
+    </div>
+   
+  )
+}
+
+export default App
